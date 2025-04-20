59821c75183d8b515d2a1a7b79f878e1ec2d5493
Dhruv <dhruvanandsingh2004@gmail.com>
2025-04-20 13:28:27 +0530

    changed some stuff

--- utils/cronJobs.js
+++ utils/cronJobs.js
@@ -8,7 +8,7 @@ const startCronJobs = () => {
   cron.schedule("0 */2 * * *", async () => {
     console.log("🔁 Running scheduled scraping job...");
     try {
-      const res = await axios.get("http://localhost:5000/api/scrape");
+      const res = await axios.get("https://scrape-backend-3zgq.onrender.com/api/scrape");
       console.log("✅ Data successfully updated!", res.status);
     } catch (error) {
       console.error("❌ Error in cron job:", error.message);
